51c6882936ba2e6a3abbc0a955d6d9f9f9eeb378
ahmedhany-1 <ahmedfathyhany@gmail.com>
2024-03-20 20:51:32 +0200

    lec1 of the day1

---
+++ .history/k_20240320205050.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct student  
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    cout << "Enter your x and y : ";
+    student s1;
+    cin >> s1.x >> s1.y;
+    cout <<"the x of the student is :"<< s1.x << " and y of the student is : "<< s1.y; 
+    return 0;
+}
