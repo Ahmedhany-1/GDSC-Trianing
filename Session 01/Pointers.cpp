42f03d9200a9da2f55e788da3eb97ed188dbe391
Ahmed Hany <ahmedfathyhany@gmail.com>
2024-03-21 00:11:15 +0200

    updated

---
+++ Session 01/Pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
+/* Session 1
+    - Pointers
+    - Static allocation && Dynamic allocation
+    -
+
+*/
+
+/*
+    Task 1
+
+*/
