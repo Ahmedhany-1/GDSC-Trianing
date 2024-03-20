7df8e4931f546b8d8b93f693851d0f78bf8c0b85
Ahmed Hany <ahmedfathyhany@gmail.com>
2024-03-20 21:13:02 +0200

    first changing

---
+++ Session 01/SessionCode.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello ya shabaab \n";
+    cout << "enter your id: ";
+    int x;
+    cin >> x;
+    cout << "Your id is " << x;
+    return 0;
+}
